c10c28f6efb03498ddcd9ad74f8920abc13d3250
Archer36 <archer36@gmail.com>
2022-11-07 14:30:49 -0500

    Encoder Fix and Custom Lighting
    Fixes the placement in VIAL for the rotary encoder.
    Adds a custom rgbmatrix animation for per key RGB

--- keyboards/keebio/bdn9/rev2/keymaps/vial/keymap.c
+++ keyboards/keebio/bdn9/rev2/keymaps/vial/keymap.c
@@ -47,3 +47,33 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    HSV hsvCyan = {127, 255, 63};
+    HSV hsvGreen = {85, 255, 63};
+    HSV hsvRed = {0, 255, 63};
+
+    RGB rgb;
+
+    if (layer_state_is(0)) {
+        // Set underglow to cyan for layer 0
+        rgb = hsv_to_rgb(hsvCyan);
+    } else if (layer_state_is(1)) {
+        // Set underglow to green for layer 1
+        rgb = hsv_to_rgb(hsvGreen);
+    } else {
+        // Set underglow to red for default/unknown
+        rgb = hsv_to_rgb(hsvRed); 
+    }
+
+    for (uint8_t i = led_min; i <= led_max; i++) {
+        if (HAS_FLAGS(g_led_config.flags[i], 0x02)) { // 0x02 == underglow
+            rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+        }
+    }
+    
+}
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_my_per_key_rgb);
+}
