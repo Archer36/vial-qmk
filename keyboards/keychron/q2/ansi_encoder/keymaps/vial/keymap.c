0656b73729218a2ce9759a1e21f76c3047402648
Archer36 <archer36@gmail.com>
2023-12-11 14:12:31 -0500

    Working layer indicators

--- keyboards/keychron/q2/ansi_encoder/keymaps/vial/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/vial/keymap.c
@@ -16,6 +16,7 @@
 
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
+#include "print.h"
 
 enum layers{
     MAC_BASE,
@@ -56,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN3] = LAYOUT_ansi_67(
         KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
-        RGB_TOG, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,            _______,          _______,
+        RGB_TOG, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,  _______,          QK_BOOT,
+        _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,            _______,          DB_TOGG,
         _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______, _______,
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______, _______, _______)
 };
@@ -78,4 +79,56 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     return true;
+}
+
+uint8_t lastLayer;
+
+void keyboard_post_init_user(void) {
+  lastLayer = get_highest_layer(layer_state);
+}
+
+HSV hsvCyan = {127, 255, 63};
+HSV hsvGreen = {85, 255, 63};
+HSV hsvRed = {0, 255, 63};
+
+RGB rgb;
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t currentLayer = get_highest_layer(layer_state);
+    switch(currentLayer) {
+        case 0:
+            RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 255);
+            break;
+        case 1:
+            RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 255);
+            break;
+        case 2:
+            RGB_MATRIX_INDICATOR_SET_COLOR(2, 0, 0, 255);
+            break;
+        case 3:
+            RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 255);
+            break;
+        case 4:
+            RGB_MATRIX_INDICATOR_SET_COLOR(4, 0, 0, 255);
+            break;
+    }
+    if (lastLayer != currentLayer) {
+        if (layer_state_is(4)) {
+            // Set rgb to cyan for layer 0
+            rgb = hsv_to_rgb(hsvCyan);
+        } else if (layer_state_is(2)) {
+            // Set underglow to green for layer 1
+            rgb = hsv_to_rgb(hsvGreen);
+        } else {
+            // Set underglow to red for default/unknown
+            rgb = hsv_to_rgb(hsvRed);
+        }
+
+        for (uint8_t i = led_min; i <= led_max; i++) {
+            if (HAS_FLAGS(g_led_config.flags[i], 0x01)) { // 0x01 == outter keys
+                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+            }
+        }
+    }
+    return false;
 }
--- keyboards/keychron/q2/ansi_encoder/keymaps/vial/rules.mk
+++ keyboards/keychron/q2/ansi_encoder/keymaps/vial/rules.mk
@@ -2,6 +2,7 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+CONSOLE_ENABLE = yes
 
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
